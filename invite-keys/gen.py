0e2c5ed1e648eec65aa9c4b4dd42b8c0c9cb0c53
Aspw-w <0kcabtuo0@gmail.com>
2024-02-03 08:00:28 +0900

    daw

--- invite-keys/gen.py
+++
@@ -1,9 +0,0 @@
-import random
-
-inputCount = int(input('何個キーを生成しますか: '))
-
-for i in range(inputCount):
-    f = open('generated.txt', 'a')
-    f.write(f'{random.randint(10000, 99999)}-{random.randint(10000, 99999)}-{random.randint(10000, 99999)}-{random.randint(10000, 99999)}-{random.randint(10000, 99999)}')
-    f.write('\n')
-    f.close()
